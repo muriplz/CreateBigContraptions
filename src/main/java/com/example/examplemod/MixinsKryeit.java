996ad04437729a098ff3f68f48f27b93c34273bd
muriplz <46825658+muriplz@users.noreply.github.com>
2022-01-31 17:29:06 +0100

    testing needed (WIP)

---
+++ src/main/java/com/example/examplemod/MixinsKryeit.java
@@ -0,0 +1,9 @@
+package com.example.examplemod;
+
+import net.minecraftforge.fml.common.Mod;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("mixinskryeit")
+public class MixinsKryeit
+{
+}
---
+++ src/main/java/com/example/examplemod/mixins/PacketInflaterMixin.java
@@ -0,0 +1,14 @@
+package com.example.examplemod.mixins;
+
+import net.minecraft.network.CompressionDecoder;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(CompressionDecoder.class)
+public class PacketInflaterMixin {
+    @ModifyConstant(method = "decode",constant = @Constant(intValue = 2097152))
+    private int newConstant(int old) {
+        return 2000000000;
+    }
+}
